4fbda81f2ce630d99f44fde8f0dba8e00b2dd71b
joaomatosq <joaomatosq@gmail.com>
2025-03-22 13:59:49 -0300

    Criando pasta components e seus respectivos arquivos + import no app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import Produtos from "./components/Produtos";
+import Sobre from "./components/Sobre";
+import Footer from "./components/Footer";
+
+
 function App() {
-  
+
   return (
-    <>
-    </>
-  )
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/produtos" element={<Produtos />} />
+        <Route path="/sobre" element={<Sobre />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App
