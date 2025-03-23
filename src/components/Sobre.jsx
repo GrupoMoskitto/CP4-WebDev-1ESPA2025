2b538ed6c2cb862bb1bfa9a068fca82572b78fc8
ana <000rand000rand@gmail.com>
2025-03-23 15:49:40 -0300

    ajustes na pag sobre

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -4,9 +4,9 @@ import "../css/Sobre.css"
 function Sobre() {
     return (
       <div>
-        <h1>Sobre Nós</h1>
+        <h1>SOBRE NÓS</h1>
         <img src="https://via.placeholder.com/300" alt="Sobre" />
-        <p>{"Lorem ipsum dolor sit amet, ".repeat(20)}</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
     );
   }
